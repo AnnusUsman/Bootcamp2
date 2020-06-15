b28051ebe846297e97fd3fdc6681e463e990894d
annususman13 <annususman42@gmail.com>
2020-06-15 21:07:48 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Bootcamp from './bootcamp.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+        <Bootcamp myName="Annus Usman"/>
+    {
+      /* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World from Annus!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/bootcamp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Bootcamp(props){
+    return(
+        <div>
+            <h1>Hello <ins>BOOTCAMP 2020</ins> team from {props.myName}!</h1>
+            <p>It's my second assignment.</p>
+        </div>
+    )
+    
+}
+
+export default Bootcamp;
